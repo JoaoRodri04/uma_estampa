91cfa172169c9a5d74990fe073d6dcd3ff4a0eba
JoaoRodri04 <joaopaulorodrigues@ipvc.pt>
2026-06-04 14:49:57 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'uma_estampa',
+  webDir: 'www'
+};
+
+export default config;
